a436b486c7053681883d8db77fc4caa2d56bd485
krpchandok <kirpachandok@gmail.com>
2026-09-07 23:34:10 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,8 @@
 Rails.application.routes.draw do
-  get "repositories/index"
-  get "repositories/show"
-  get "repositories/update"
-  get "dashboard/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "dashboard#index"
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  post "/webhooks/github", to: "webhooks#github"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  Rails.application.routes.draw do
-  get "repositories/index"
-  get "repositories/show"
-  get "repositories/update"
-  get "dashboard/index"
-    post "/webhooks/github", to: "webhooks#github"
-  end
-
-  Rails.application.routes.draw do
-  get "repositories/index"
-  get "repositories/show"
-  get "repositories/update"
-    post "/webhooks/github", to: "webhooks#github"
-
-    root "repositories#index"
-    resources :repositories, only: [:index, :show, :update]
-  end
+  root "repositories#index"
+  resources :repositories, only: [:index, :show, :update]
 end
